0d984e91903e56a2e98b66a2929bd86df8f15800
mcomis <gabba123>
2015-11-19 22:04:47 +0100

    Rejected commit: Default

--- Backend/src/main/java/User.java
+++ Backend/src/main/java/User.java
@@ -8,6 +8,11 @@ public class User {
     private String firstName;
     private Location home;
 
+    public User(String firstName, Location home) {
+        this.firstName = firstName;
+        this.home = home;
+    }
+
     public String getFirstName() {
         return firstName;
     }
