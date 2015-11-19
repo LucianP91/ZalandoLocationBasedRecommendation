533fec68f48f53a08cae311e1abb5a291ee05e6c
Johannes <johannes.lipp@rwth-aachen.de>
2015-11-19 22:48:16 +0100

    Added JSON Parser JAR

--- Backend/src/com/hackathon/Main.java
+++ Backend/src/com/hackathon/Main.java
@@ -1,8 +1,9 @@
-package com.hackathon;
+package Backend.src.com.hackathon;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Backend.");
+        ApiConnection.curlURL("https://api.zalando.com/articles?category=cocktail-dresses&page=1&pageSize=10");
     }
 }
