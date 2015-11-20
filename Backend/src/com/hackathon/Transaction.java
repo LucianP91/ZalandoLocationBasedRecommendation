b139ac08f9dbc2a8e9978b8b8f8e97f5c71b708e
mcomis <gabba123>
2015-11-20 03:36:20 +0100

    User added

--- Backend/src/com/hackathon/Transaction.java
+++ Backend/src/com/hackathon/Transaction.java
@@ -3,9 +3,16 @@ import Backend.src.com.hackathon.Location;
 import java.util.Calendar;
 /**
  * Created by Marciana on 19.11.2015.
+ * Representation of an Transaction
  */
 public class Transaction {
+    /**
+     * Time of the transaction
+     */
     Calendar transactionTime;
+    /**
+     * Customer who initialized transaction
+     */
     User customer;
 
     public Transaction(User customer) {
