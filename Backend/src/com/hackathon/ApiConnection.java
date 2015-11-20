649a5c96a7401ebcd2b33f627b4af47d79fbe737
Johannes <johannes.lipp@rwth-aachen.de>
2015-11-20 08:40:44 +0100

    Fixed price string

--- Backend/src/com/hackathon/ApiConnection.java
+++ Backend/src/com/hackathon/ApiConnection.java
@@ -116,7 +116,7 @@ public class ApiConnection {
         final JSONArray imageArr =(JSONArray)((JSONObject)dress.get("media")).get("images");
         if (hasPrice) {
             final JSONObject priceObj = (JSONObject) ((JSONObject) ((JSONArray) dress.get("units")).get(0)).get("price");
-            price = ("" + priceObj.get("formatted")).replaceAll("\u00A3", "Pounds ");
+            price = ("" + priceObj.get("formatted")).substring(1);
         } else {
             price = "";
         }
