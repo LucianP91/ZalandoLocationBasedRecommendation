4134dad6ab2bf029f805590a9085905373e6bd91
mcomis <gabba123>
2015-11-20 01:49:16 +0100

    User added

--- Backend/src/com/hackathon/Location.java
+++ Backend/src/com/hackathon/Location.java
@@ -4,37 +4,41 @@ package Backend.src.com.hackathon;
  * Created by Marciana on 19.11.2015.
  */
 public class Location {
-    private int x,y;
-
-    public Location(int x, int y) {
-        this.x = x;
-        this.y = y;
+    private double longitude, latitude;
+    private final int R = 6373;
+    public Location( double latitude, double longitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
-    public int getX() {
+    public double getLongitude() {
 
-        return x;
+        return longitude;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
     }
 
-    public int getY() {
-        return y;
+    public double getLatitude() {
+        return latitude;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
     }
 
     public double getDistance(Location to){
-      return  Math.sqrt( Math.pow(to.getX()-this.getX(),2) + Math.pow(to.getY()-this.getY(),2));
+       double dlon=Math.toRadians(to.getLongitude()-this.getLongitude());
+        double dlat= Math.toRadians(to.getLatitude()-this.getLatitude());
+        double a=Math.pow(Math.sin(dlat/2),2)+Math.cos(Math.toRadians(to.getLatitude()))*Math.cos(Math.toRadians(this.getLatitude()))*Math.pow(Math.sin(dlon/2),2);
+        double c = 2* Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        return R *c;
     }
 
  public static void main(String[] args){
-     Location l1= new Location(0,0);
-     Location l2 = new Location(5,2);
-     System.out.println(l1.getDistance(l2));
+     Location Aachen= new Location(50.7753455,6.0838868);
+     Location Berlin = new Location(52.519171,13.4060912);
+     System.out.println(Aachen.getDistance(Berlin));
  }
 }
