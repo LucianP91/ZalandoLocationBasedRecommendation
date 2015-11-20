ed40e38b0bac392fce46e67d132023f41bb927a7
Johannes <johannes.lipp@rwth-aachen.de>
2015-11-20 01:14:39 +0100

    Added IP to IpDetails conversion in ApiConnection and test call in main

--- Backend/src/com/hackathon/Main.java
+++ Backend/src/com/hackathon/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Backend.");
-        ApiConnection.curlURL(APIURL);
+        //ApiConnection.curlURL(APIURL);
+        System.out.println(ApiConnection.getIpDetails("134.61.129.229"));
     }
 }
