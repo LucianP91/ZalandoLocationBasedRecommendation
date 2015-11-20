4215af99531b16d4fb4cef4204fbd8979aebb04a
Johannes <johannes.lipp@rwth-aachen.de>
2015-11-20 03:36:03 +0100

    Small change in main

--- Backend/src/com/hackathon/Main.java
+++ Backend/src/com/hackathon/Main.java
@@ -1,7 +1,7 @@
 package Backend.src.com.hackathon;
 
 public class Main {
-    private final static String APIURL = "https://api.zalando.com/articles?category=cocktail-dresses&page=1&pageSize=10";
+    public final static String APIURL = "https://api.zalando.com/articles?category=cocktail-dresses&page=1&pageSize=10";
 
     public static void main(String[] args) {
         System.out.println("Hello Backend.");
